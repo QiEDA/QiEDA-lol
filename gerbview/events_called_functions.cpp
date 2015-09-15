8e6db14a7d716f442a2d71973f7671494b606b2f
Mark Roszko <mark.roszko@gmail.com>
2015-09-14 20:01:21 -0400

    Allow changing order of layers. Tweak some function names.

--- gerbview/events_called_functions.cpp
+++ gerbview/events_called_functions.cpp
@@ -222,7 +222,7 @@ void GERBVIEW_FRAME::Process_Special_Functions( wxCommandEvent& event )
 
 void GERBVIEW_FRAME::OnSelectActiveDCode( wxCommandEvent& event )
 {
-    GERBER_IMAGE* gerber_image = g_GERBER_List.GetGbrImage( getActiveLayer() );
+    GERBER_IMAGE* gerber_image = g_GERBER_List.GetGerberByListIndex( getActiveLayer() );
 
     if( gerber_image )
     {
@@ -254,7 +254,7 @@ void GERBVIEW_FRAME::OnSelectActiveLayer( wxCommandEvent& event )
 void GERBVIEW_FRAME::OnShowGerberSourceFile( wxCommandEvent& event )
 {
     int     layer = getActiveLayer();
-    GERBER_IMAGE* gerber_layer = g_GERBER_List.GetGbrImage( layer );
+    GERBER_IMAGE* gerber_layer = g_GERBER_List.GetGerberByListIndex( layer );
 
     if( gerber_layer )
     {
